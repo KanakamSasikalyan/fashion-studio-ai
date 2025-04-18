39b92290ed8d514890630a753c6d07ee32e43e3b
KanakamSasikalyan <kanakamsasikalyan1@gmail.com>
2025-04-18 17:38:28 +0530

    modified design controller

--- src/main/java/io/metaverse/fashion/studio/controller/DesignController.java
+++ src/main/java/io/metaverse/fashion/studio/controller/DesignController.java
@@ -14,6 +14,7 @@ import java.nio.charset.StandardCharsets;
 
 @RestController
 @RequestMapping("/api/designs")
+@CrossOrigin(origins = "*") // Allow all origins
 public class DesignController {
 
     private final AIClothingService aiService;
