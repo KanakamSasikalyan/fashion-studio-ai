47b69543aa83241e7154b979a365bf70d131bfc3
KanakamSasiKalyan <kanakamsasikalyan1@gmail.com>
2025-05-18 16:21:56 +0530

    added data.sql , #Error

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO clothing_designs (image_url, prompt, style, gender) VALUES
+('https://ik.imagekit.io/sp7ub8zm6/fashion_designs/design_0006.jpg', 'ZARA Clothing Images#6', 'Casual', 'Male'),
+('https://ik.imagekit.io/sp7ub8zm6/fashion_designs/design_0005.jpg', 'ZARA Clothing Images#5', 'Casual', 'Male'),
+('https://ik.imagekit.io/sp7ub8zm6/fashion_designs/design_0004.jpg', 'ZARA Clothing Images#4', 'Casual', 'Male'),
+('https://ik.imagekit.io/sp7ub8zm6/fashion_designs/design_0003.jpg', 'ZARA Clothing Images#3', 'Casual', 'Male'),
+('https://ik.imagekit.io/sp7ub8zm6/fashion_designs/design_0002.jpg', 'ZARA Clothing Images#2', 'Casual', 'Male'),
+('https://ik.imagekit.io/sp7ub8zm6/fashion_designs/design_0001.jpg', 'ZARA Clothing Images#1', 'Casual', 'Male'),
+('https://ik.imagekit.io/sp7ub8zm6/fashion_designs/female_0001.jpg', 'ZARA Female Clothing', 'Formal', 'Female'),
+('https://ik.imagekit.io/sp7ub8zm6/fashion_designs/female_0002.jpg', 'ZARA Female Clothing', 'Formal', 'Female'),
+('https://ik.imagekit.io/sp7ub8zm6/fashion_designs/female_0003.jpg', 'ZARA Female Clothing', 'Formal', 'Female');
+
+INSERT INTO users(username, email, password) values('admin', 'admin@gmail.com', 'password');
