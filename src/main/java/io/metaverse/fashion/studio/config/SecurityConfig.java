70d1e41328f17600b20a6199fb30b0b44969c7b0
KanakamSasikalyan <kanakamsasikalyan1@gmail.com>
2025-08-01 00:47:17 +0530

    updated security config

--- src/main/java/io/metaverse/fashion/studio/config/SecurityConfig.java
+++ src/main/java/io/metaverse/fashion/studio/config/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
         configuration.setAllowedOriginPatterns(List.of(
                 "http://localhost:3000",
                 "http://localhost:8080",
-                "https://ubc-fashion-studio-ai-frontend-ui-hdhxfvguftaba2fx.centralus-01.azurewebsites.net" // Add this line
+                "https://ubc-fashion-studio-ai-frontend-container-efadeqfwfkfgg4dm.canadacentral-01.azurewebsites.net" // Add this line
         ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
